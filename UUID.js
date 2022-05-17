40e9a10a1ecd48e74e1860a55fd776ede818beab
Juan <83260421+JuDaSe@users.noreply.github.com>
2022-05-17 19:21:53 -0300

    testibec

---
+++ UUID.js
@@ -0,0 +1,15 @@
+function generateUuid(){
+    let now = new Date().getTime();
+    let uuid = 'xxyx-yyyy-xxyx-yyxy'.replace(/[xy]/g, function(c){
+        let aleatorio = (now + Math.random() * 6) % 36 | 0;
+        now = Math.floor(now / 6);
+
+        return (c == 'x' ? aleatorio : (aleatorio & 0x3 | 0x8)).toString(36)
+    });
+    return uuid;
+}
+
+for(let i = 1; i <= 10; ++i){
+    console.log(generateUuid());
+}
+
