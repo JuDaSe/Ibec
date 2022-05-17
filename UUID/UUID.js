cb408e79dd77e28f3ff37ed6bdec6ac5f02d3d83
Juan <83260421+JuDaSe@users.noreply.github.com>
2022-05-17 19:40:42 -0300

    Identificador Universal

--- UUID/UUID.js
+++ UUID/UUID.js
@@ -1,5 +1,5 @@
 function generateUuid(){
-    let now = new Date().getTime();
+    let now = new Date().getTime() ;
     let uuid = 'xxyx-yyyy-xxyx-yyxy'.replace(/[xy]/g, function(c){
         let aleatorio = (now + Math.random() * 6) % 36 | 0;
         now = Math.floor(now / 6);
@@ -12,4 +12,6 @@ function generateUuid(){
 for(let i = 1; i <= 10; ++i){
     console.log(generateUuid());
 }
+
+// Prueba
 
